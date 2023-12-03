4734a8d504e4aa8ad00ffcd3ae89282aa413abd0
fabigazi <fgaziano98@gmail.com>
2023-12-03 00:24:14 -0500

    PGD stoping point

--- PGD_attack.py
+++ PGD_attack.py
@@ -1,3 +1,4 @@
+from matplotlib import pyplot as plt
 import torch
 import torch.nn as nn
 from classify_model import LeNet5
@@ -14,7 +15,7 @@ def PGD(model, image, label, epsilon, iterations, alpha):
 		criterion = nn.CrossEntropyLoss()
 
 		output = model(perturbed_image)
-		
+
 		loss = criterion(output, label)
 		loss.backward()
 
@@ -40,10 +41,30 @@ train_loader = DataLoader(train, batch_size=64, shuffle=True)
 
 # print(max(train[0][0].flatten()))
 target_model = TargetModel(model)
-print(target_model.predict(train[0][0]))
 
 epsilon = 0.05
 iterations = 500
 alpha = 2/255
-# attack = PGD(model, train[0][0], , epsilon, iterations, alpha)
+
+results = []
+
+for batch, (X, y) in enumerate(train_loader):
+	attack = PGD(model, X, y, epsilon, iterations, alpha)
+	results.append(attack)
+	break
+
+print(results[0][0].shape)
+
+fig = plt.figure()
+ax = fig.add_subplot(1, 2, 1)
+ax.imshow(train[0][0].squeeze(0), cmap="gray")
+ax.set_title("Original Image " + "pred: " + str(target_model.predict(train[0][0])))
+ax = fig.add_subplot(1, 2, 2)
+ax.imshow((train[0][0] + attack[0][0].detach().numpy()).squeeze(0), cmap="gray")
+ax.set_title(
+    "Poisoned Image "
+    + "pred: "
+    + str(target_model.predict(train[0][0] + attack[0][0].detach().numpy()))
+)
+plt.show()
 
