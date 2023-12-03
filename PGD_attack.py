893a276750b4a4c8064d8d7677b2cded0fa0e19d
KodeJaiSurya <59638236+KodeJaiSurya@users.noreply.github.com>
2023-12-02 22:21:05 -0500

    Update PGD_attack.py

--- PGD_attack.py
+++ PGD_attack.py
@@ -1,5 +1,10 @@
 import torch
 import torch.nn as nn
+from classify_model import LeNet5
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision import transforms
+from target_model import TargetModel
 
 def PGD(model, image, label, epsilon, iterations, alpha):
 
@@ -20,3 +25,25 @@ def PGD(model, image, label, epsilon, iterations, alpha):
 		perturbed_image.grad.zero_()
 
 	return perturbed_image
+
+model = LeNet5()
+model.load_state_dict(torch.load("model_1.pth"))
+model.eval()
+
+train = datasets.MNIST(
+    root="dataset/",
+    train=True,
+    download=True,
+    transform=transforms.Compose([transforms.Resize((32, 32)), transforms.ToTensor()]),
+)
+train_loader = DataLoader(train, batch_size=64, shuffle=True)
+
+# print(max(train[0][0].flatten()))
+target_model = TargetModel(model)
+print(target_model.predict(train[0][0]))
+
+epsilon = 0.05
+iterations = 500
+alpha = 2/255
+# attack = PGD(model, train[0][0], , epsilon, iterations, alpha)
+
